c5c4ab75ec90969c47c324278d23e5ce230cd12d
Auwnsullem <auwnsullem@gmail.com>
2020-10-06 14:32:49 +0700

    Updates

---
+++ precache-manifest.9a36ec7250b7cd0bfa7500e2a2f4a15e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b63054d7f0dc32483b25733844678d",
+    "url": "/quotes/index.html"
+  },
+  {
+    "revision": "d6602198899e97617d89",
+    "url": "/quotes/static/css/main.e75c12d1.chunk.css"
+  },
+  {
+    "revision": "d344767fcf60a7affbbc",
+    "url": "/quotes/static/js/2.8f046f0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quotes/static/js/2.8f046f0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6602198899e97617d89",
+    "url": "/quotes/static/js/main.ba78b16e.chunk.js"
+  },
+  {
+    "revision": "90cc41a9d6724a16f105",
+    "url": "/quotes/static/js/runtime-main.91a153d2.js"
+  }
+]);
